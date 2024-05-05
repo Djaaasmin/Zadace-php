ac83ddf1fd236fe15dbcaeb36fde0a916b2e4fd4
Djaaasmin <125029966+Djaaasmin@users.noreply.github.com>
2024-05-05 22:11:57 +0200

    Create zadaca.php

---
+++ zadaca.php
@@ -0,0 +1,25 @@
+<?php 
+$postaniProgramer="Postani programer";
+$navigacija = ["glavna","o nama","kontakt"];
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <title><?= $postaniProgramer   ?> </title>
+</head>
+<body>
+    <h1><?= $postaniProgramer   ?> </h1>
+    <nav>
+        <a href=""><?= $navigacija[0] ?> </a>
+        <a href=""><?= $navigacija[1] ?></a>
+        <a href=""><?= $navigacija[2] ?></a>
+    </nav>
+    
+</body>
+</html>
