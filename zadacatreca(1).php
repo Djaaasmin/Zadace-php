c11347571d23af3cca0d3628d8db53993a317c30
Djaaasmin <125029966+Djaaasmin@users.noreply.github.com>
2024-05-13 00:47:49 +0200

    Create zadacatreca(1).php

---
+++ zadacatreca(1).php
@@ -0,0 +1,17 @@
+<?php 
+
+$ime = "administrator";
+$lozinka = "mOjaSifrajeSigurna";
+
+
+
+if ($ime =="administrator"  &&  $lozinka = "mOjaSifrajeSigurna"){
+    echo "Dobro došli administratoru ";
+}
+else {
+    echo "Pogrešno korisničko ime ili lozinka";
+} 
+?>
+
+
+
