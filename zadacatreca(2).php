12778dca470cc37b2c426fbc9c4c37bd1df8be78
Djaaasmin <125029966+Djaaasmin@users.noreply.github.com>
2024-05-13 00:47:44 +0200

    Create zadacatreca(2).php

---
+++ zadacatreca(2).php
@@ -0,0 +1,17 @@
+<?php 
+$trenutnoVrijeme=18;
+
+if ($trenutnoVrijeme >=5  &&  $trenutnoVrijeme <12 ){
+    echo "Dobro Jutro";
+}
+else if ($trenutnoVrijeme >=12  &&  $trenutnoVrijeme <20 ){
+    echo "Dobar Dan";
+}
+else {
+    echo "Dobro vece";
+}
+
+
+
+
+?>
